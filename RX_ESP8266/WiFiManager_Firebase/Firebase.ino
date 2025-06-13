3be7318469a7bda52480bc2abf9f53614bf3502a
Dalilo Yogi Essy Pratama <94462659+daliloyogi@users.noreply.github.com>
2025-06-13 23:08:39 +0700

    Update Firebase.ino
    adding sendNotif function

--- RX_ESP8266/WiFiManager_Firebase/Firebase.ino
+++ RX_ESP8266/WiFiManager_Firebase/Firebase.ino
@@ -82,3 +82,67 @@ bool setNotification(FirebaseData &fbdo, const String &path, std::initializer_li
     return false;
   }
 }
+
+void sendNotif() {
+  //Suhu
+  if (temperature > 25.0 && !notifSuhu) {
+    //Upload Notifikasi Suhu
+    String notifikasiPath = "/Siponik/" + String(ID_ESP) + "/Notifikasi";
+    String Pesan = "Suhu air terlalu tinggi (" + String(temperature) + (char)176 + "C). Suhu harus di angka 15-25" + (char)176 + "C. Sesuaikan pendinginan.";
+    String Judul = "Peringatan Suhu";
+    notifSuhu = setNotification(fbdo, notifikasiPath, { { "title", Judul }, { "message", Pesan } });
+  } else if (temperature < 15.0 && !notifSuhu) {
+    //Upload Notifikasi Suhu
+    String notifikasiPath = "/Siponik/" + String(ID_ESP) + "/Notifikasi";
+    String Pesan = "Suhu air terlalu rendah (" + String(temperature) + (char)176 + "C). Suhu harus di angka 15-25" + (char)176 + "C. Sesuaikan pendinginan.";
+    String Judul = "Peringatan Suhu";
+    notifSuhu = setNotification(fbdo, notifikasiPath, { { "title", Judul }, { "message", Pesan } });
+  } else {
+    notifSuhu = false;
+  }
+
+  //Nutrisi
+  if (nutrisi < 560 && !notifNutrisi) {
+    //Upload Notifikasi Nutrisi
+    String notifikasiPath = "/Siponik/" + String(ID_ESP) + "/Notifikasi";
+    String Pesan = "Kadar nutrisi rendah (" + String(nutrisi) + " ppm). Kadar nutrisi harus berada di angka 560-840 ppm. Pompa air nutrisi dihidupkan otomatis.";
+    String Judul = "Peringatan Kadar Nutrisi";
+    notifNutrisi = setNotification(fbdo, notifikasiPath, { { "title", Judul }, { "message", Pesan } });
+  } else if (nutrisi > 840 && !notifNutrisi) {
+    //Upload Notifikasi Nutrisi
+    String notifikasiPath = "/Siponik/" + String(ID_ESP) + "/Notifikasi";
+    String Pesan = "Kadar nutrisi terlalu tinggi (" + String(nutrisi) + " ppm). Kadar nutrisi harus berada di angka 560-840 ppm. Sesuaikan kadar nutrisi.";
+    String Judul = "Peringatan Kadar Nutrisi";
+    notifNutrisi = setNotification(fbdo, notifikasiPath, { { "title", Judul }, { "message", Pesan } });
+  } else {
+    notifNutrisi = false;
+  }
+
+  //pH
+  if (pH < 6.0 && !notifPH) {
+    //Upload Notifikasi pH
+    String notifikasiPath = "/Siponik/" + String(ID_ESP) + "/Notifikasi";
+    String Pesan = "pH turun di bawah batas minimum (pH " + String(pH) + "). pH harus berada di angka 6-7. Hidupkan pompa nutrisi";
+    String Judul = "Peringatan Nilai pH";
+    notifPH = setNotification(fbdo, notifikasiPath, { { "title", Judul }, { "message", Pesan } });
+  } else if (pH > 7.0 && !notifPH) {
+    //Upload Notifikasi pH
+    String notifikasiPath = "/Siponik/" + String(ID_ESP) + "/Notifikasi";
+    String Pesan = "pH terlalu tinggi  (pH " + String(pH) + "). pH harus berada di angka 6-7. Sesuaikan nutrisi";
+    String Judul = "Peringatan Nilai pH";
+    notifPH = setNotification(fbdo, notifikasiPath, { { "title", Judul }, { "message", Pesan } });
+  } else {
+    notifPH = false;
+  }
+
+  //Air
+  if (waterLevel < 40 && !notifAir) {
+    //Upload Notifikasi Water Level
+    String notifikasiPath = "/Siponik/" + String(ID_ESP) + "/Notifikasi";
+    String Pesan = "Stok air dalam tandon sedikit. Hidupkan pompa otomatis.";
+    String Judul = "Peringatan Stok Air Tandon";
+    notifAir = setNotification(fbdo, notifikasiPath, { { "title", Judul }, { "message", Pesan } });
+  } else {
+    notifAir = false;
+  }
+}
